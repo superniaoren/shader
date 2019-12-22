b5022e09d14042eda7f2d556f75545bc3802677c
superniaoren <superniaoren@yahoo.com>
2019-12-22 22:21:21 +0800

    continue update test create_window, add glviewport

--- OpenGL-Blockhead/create-window/test_create_window.cpp
+++ OpenGL-Blockhead/create-window/test_create_window.cpp
@@ -2,12 +2,31 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+#include <GL/gl.h>
+
 int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
     glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learning create window", NULL, NULL);
+	if (window == NULL) {
+		std::cerr << "Failed to create glfw window. " << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// opengl
+	//glViewPoint(0, 0, 800, 600);  // [zoo] error, port, not point, just a funny typo ...
+	glViewport(0, 0, 800, 600);
+
+	// close
+	glfwTerminate();
 
 	return 0;
 }
